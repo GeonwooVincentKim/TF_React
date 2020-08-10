e182c2dc6a42bb13f962c6603063d3b58d1c7147
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-10 21:37:45 +0900

    Created TensorflowExample.js and .css file.

---
+++ first-tensorflow-react.js/src/Components/TensorflowExample.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+import './TensorflowExample.css';
+
+const TensorflowExample = () => {
+    return (
+        <div className="tensorflow-example">
+
+        </div>
+    );
+}
+
+export default TensorflowExample;
