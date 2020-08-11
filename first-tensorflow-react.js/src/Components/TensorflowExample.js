54d09a878dbc5a044d6f0b8dd613c2fbbff6d20c
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-11 10:36:37 +0900

    Set the value into const [valuePairsState, setValuePairsState]

--- first-tensorflow-react.js/src/Components/TensorflowExample.js
+++ first-tensorflow-react.js/src/Components/TensorflowExample.js
@@ -7,7 +7,13 @@ const TensorflowExample = () => {
     // Value pairs state
     const [valuePairsState, setValuePairsState] = useState([
         {x: -1, y: -3},
-    ])
+        {x: 0., y: -1},
+        {x: 1, y: 1},
+        {x: 2, y: 3},
+        {x: 3, y: 5},
+        {x: 4, y: 7},
+    ]);
+
     return (
         <div className="tensorflow-example">
 
