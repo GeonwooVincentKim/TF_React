075b937b068cce0f581127bd6a488e234bc14778
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-11 10:34:37 +0900

    Imported and apply modules into App.js.

--- first-tensorflow-react.js/src/Components/TensorflowExample.js
+++ first-tensorflow-react.js/src/Components/TensorflowExample.js
@@ -1,8 +1,13 @@
 import React, {useState} from 'react';
+import update from 'immutability-helper';
 
 import './TensorflowExample.css';
 
 const TensorflowExample = () => {
+    // Value pairs state
+    const [valuePairsState, setValuePairsState] = useState([
+        {x: -1, y: -3},
+    ])
     return (
         <div className="tensorflow-example">
 
